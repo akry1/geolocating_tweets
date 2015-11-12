fa1c88a4d0aa5e3539ffcbfcda7c5b0966285d49
Aravinda Kumar Reddy Yempada <aravind_yempada@outlook.com>
2015-11-12 02:51:39 -0700

    word-location counts

--- loadFiles.py
+++ loadFiles.py
@@ -9,6 +9,7 @@ import os.path as op
 import pandas as pd
 import json
 import geocoder
+import re
 
 def loadfiles(path):
     directories = [path]
@@ -31,20 +32,45 @@ def loadfiles(path):
     return  pd.DataFrame(tweetdata,columns=['text','lat','lng'])
 
 
-def reverseGeocode(lng,lat):
+def reverseGeocode(lat,lng):
     #g =  geocoder.w3w([lat,lng],method='reverse',key='EBFZ9ETX')    
     #return '-'.join(g.json.get('words',''))
     g =  geocoder.google([lat,lng],method='reverse')    
     return g.city if g.city else g.state
     
-
+    
+def mapLocation(lat,lng):
+    def convert(x):
+        x = int(x)
+        diff = abs(x%5)
+#        if  diff > 5:
+#            x += 5 - diff  
+#        else:
+#            x -= diff
+        return str(x-diff)
+    return ':'.join([convert(lng),convert(lat)])
+    
+    
+wordLocDict = {}    
+def wordsForChiFeatures(text,loc):
+    notAlphaNumeric = u'[^a-z0-9]'
+    for i in text.split(' '):
+        i= i.lower()
+        if len(i)>3 :
+            if not re.search(notAlphaNumeric,i):
+                wordLocDict[(i,loc)] = wordLocDict.get((i,loc),0) + 1
+                
 
 #loadfiles('C:\Users\AravindKumarReddy\Downloads\SMMProject2')
 def main():
     tweetdata = loadfiles('C:\Users\AravindKumarReddy\Downloads\SMMSample')
-    tweetdata['w3wCode'] = map(reverseGeocode, tweetdata['lng'],tweetdata['lat'])
+    #tweetdata['location'] = map(reverseGeocode, tweetdata['lat'],tweetdata['lng'])
+    tweetdata['location'] = map(mapLocation, tweetdata['lat'],tweetdata['lng'])    
+
+    map(wordsForChiFeatures,tweetdata['text'], tweetdata['location'])
     
-    print tweetdata
+    
+    print { i:j for i,j in wordLocDict.items() if j>1 }
     
 if __name__ == '__main__':
     main()
